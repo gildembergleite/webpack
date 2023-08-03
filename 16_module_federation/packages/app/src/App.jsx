f0b30631895c557ff3a56abbe59e8f306f3739ed
Gildemberg <contato@gildembergleite.com.br>
2023-08-03 09:47:31 -0300

    error in microfrontends imports

--- 16_module_federation/packages/app/src/App.jsx
+++ 16_module_federation/packages/app/src/App.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import { Link, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import React, { Suspense } from 'react'
+import { Link, BrowserRouter, Route, Routes } from 'react-router-dom'
 import './app.css'
 
 export default function App() {
    // Module Federation
-   const Home = React.lazy(() => import('HomeApp/HomePage'))
-   const Contact = React.lazy(() => import('ContactApp/ContactPage'))
+   const HomePage = React.lazy(() => import('HomeApp/HomePage'))
+   const ContactPage = React.lazy(() => import('ContactApp/ContactPage'))
 
    return (
-      <Router>
-         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <BrowserRouter basename="/">
+         <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600 px-8">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                <Link to="/" className="flex items-center gap-4">
                   <svg
@@ -36,38 +35,6 @@ export default function App() {
                      Module Federation
                   </span>
                </Link>
-               <div className="flex md:order-2">
-                  <button
-                     type="button"
-                     className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  >
-                     Get started
-                  </button>
-                  <button
-                     data-collapse-toggle="navbar-sticky"
-                     type="button"
-                     className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                     aria-controls="navbar-sticky"
-                     aria-expanded="false"
-                  >
-                     <span className="sr-only">Open main menu</span>
-                     <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                     >
-                        <path
-                           stroke="currentColor"
-                           strokeLinecap="round"
-                           strokeLinejoin="round"
-                           strokeWidth="2"
-                           d="M1 1h15M1 7h15M1 13h15"
-                        />
-                     </svg>
-                  </button>
-               </div>
                <div
                   className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                   id="navbar-sticky"
@@ -95,10 +62,12 @@ export default function App() {
                </div>
             </div>
          </nav>
-         <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-         </Routes>
-      </Router>
+         <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+               <Route exact path="/" element={<HomePage />} />
+               <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+         </Suspense>
+      </BrowserRouter>
    )
 }
